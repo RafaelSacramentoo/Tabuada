6670885f0dc0b28a3d7f8dcb2276143a97a6a7d5
Rafael Sacramento <51892364+RafaelSacramentoo@users.noreply.github.com>
2020-05-20 22:15:52 -0300

    Add files via upload

---
+++ TABUADA EM PYTHON.py
@@ -0,0 +1,16 @@
+#TABUADA
+
+linha = 1
+coluna=1
+while linha <=10:
+    while coluna <= 10:
+        print(f'{linha}  x {coluna} = {linha * coluna} ')
+        
+        coluna = coluna + 1
+        
+    linha = linha + 1
+    coluna=1
+
+
+
+
